fb3550f2ac4135033c68b94db61199a8c9ef999b
alanfabri98 <60014599+alanfabri98@users.noreply.github.com>
2021-09-25 02:45:37 -0500

    Controlador de articulo

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Article;
+//use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,25 +15,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//return $request->user();
-//});
-
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+/*
 Route::get('articles', function () {
     return Article::all();
 });
+
 Route::get('articles/{id}', function ($id) {
     return Article::find($id);
 });
+
 Route::post('articles', function (Request $request) {
     return Article::create($request->all());
 });
+
 Route::put('articles/{id}', function (Request $request, $id) {
     $article = Article::findOrFail($id);
     $article->update($request->all());
     return $article;
 });
+
 Route::delete('articles/{id}', function ($id) {
     Article::find($id)->delete();
     return 204;
 });
+*/
+
+Route::get('articles', 'App\Http\Controllers\ArticleController@index');
+Route::get('articles/{article}', 'App\Http\Controllers\ArticleController@show');
+Route::post('articles', 'App\Http\Controllers\ArticleController@store');
+Route::put('articles/{article}', 'App\Http\Controllers\ArticleController@update');
+Route::delete('articles/{article}', 'App\Http\Controllers\ArticleController@delete');
